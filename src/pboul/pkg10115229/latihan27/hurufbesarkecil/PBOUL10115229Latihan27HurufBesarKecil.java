64c800c44f7249304c2b06c8f3a01254e8d97e04
fakhri ramdani <muhammadlukman09@gmail.com>
2019-10-10 21:27:55 +0700

    Project keduapuluhtujuh

---
+++ src/pboul/pkg10115229/latihan27/hurufbesarkecil/PBOUL10115229Latihan27HurufBesarKecil.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan27.hurufbesarkecil;
+
+
+
+/**
+ *
+ * @author Lukman
+ * NIM      : 10115229
+ * NAMA     : MUHAMMAD LUKMAN HAKIM
+ * KELAS    : PBO ULANG
+ */
+public class PBOUL10115229Latihan27HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    String asli = "aPA aKu dI MArs?";
+    
+    String hurufBesar = asli.toUpperCase();
+    String hurufKecil = asli.toLowerCase();
+    
+        System.out.println("Masukkan Kalimat: "+ asli);
+        System.out.println("String huruf besar : " +hurufBesar);
+        System.out.println("String huruf kecil : " +hurufKecil);
+        
+        System.out.println("");
+        System.out.println(" develop BY: Muhammad Lukman Hakim ");
+        
+        
+    }
+    
+}
